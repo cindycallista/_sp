69b7cbe02aa75ed26c5f2b3aab2a043c806505a2
莉 <149644616+cindycallista@users.noreply.github.com>
2025-06-07 19:08:45 +0800

    Create HW3.c

---
+++ HW3.c
@@ -0,0 +1,109 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <memory.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <stdint.h>
+
+#define int long long
+
+char *p, *lp, *data;
+int *e, *le, *id, *sym, tk, ival, ty, loc, line, src, debug;
+
+enum {
+  Num = 128, Fun, Sys, Glo, Loc, Id,
+  Char, Else, Enum, If, Int, Return, Sizeof, For, Do, While,
+  Assign, Cond, Lor, Lan, Or, Xor, And, Eq, Ne, Lt, Gt, Le, Ge, Shl, Shr, Add, Sub, Mul, Div, Mod, Inc, Dec, Brak
+};
+
+enum { 
+  LLA ,IMM ,JMP ,JSR ,BZ  ,BNZ ,ENT ,ADJ ,LEV ,LI  ,LC  ,SI  ,SC  ,PSH ,
+  OR  ,XOR ,AND ,EQ  ,NE  ,LT  ,GT  ,LE  ,GE  ,SHL ,SHR ,ADD ,SUB ,MUL ,DIV ,MOD ,
+  OPEN,READ,CLOS,PRTF,MALC,FREE,MSET,MCMP,EXIT 
+};
+
+enum { CHAR, INT, PTR };
+enum { Tk, Name, Len, Class, Type, Val, HClass, HType, HVal, Idsz };
+
+void next() {
+  char *pp;
+  while (tk = *p) {
+    ++p;
+    if (tk == '\n') {
+      if (src) {
+        printf("%d: %.*s", line, p - lp, lp);
+        lp = p;
+        while (le < e) {
+          printf("  %d: %8.4s", le+1, &"LLA ,IMM ,JMP ,JSR ,BZ  ,BNZ ,ENT ,ADJ ,LEV ,LI  ,LC  ,SI  ,SC  ,PSH ,"
+                               "OR  ,XOR ,AND ,EQ  ,NE  ,LT  ,GT  ,LE  ,GE  ,SHL ,SHR ,ADD ,SUB ,MUL ,DIV ,MOD ,"
+                               "OPEN,READ,CLOS,PRTF,MALC,FREE,MSET,MCMP,EXIT,"[*++le * 5]);
+          if (*le <= ADJ) printf("%d\n", *++le); else printf("\n");
+        }
+      }
+      ++line;
+    }
+    else if (tk == '#') {
+      while (*p != 0 && *p != '\n') ++p;
+    }
+    else if ((tk >= 'a' && tk <= 'z') || (tk >= 'A' && tk <= 'Z') || tk == '_') {
+      pp = p - 1;
+      while ((*p >= 'a' && *p <= 'z') || (*p >= 'A' && *p <= 'Z') || (*p >= '0' && *p <= '9') || *p == '_')
+        p++;
+      id = sym;
+      while (id[Tk]) {
+        if (p-pp == id[Len] && !memcmp((char *)id[Name], pp, p - pp)) { tk = id[Tk]; return; }
+        id = id + Idsz;
+      }
+      id[Name] = (int)pp;
+      id[Len] = p-pp;
+      tk = id[Tk] = Id;
+      return;
+    }
+    else if (tk >= '0' && tk <= '9') {
+      if (ival = tk - '0') { while (*p >= '0' && *p <= '9') ival = ival * 10 + *p++ - '0'; }
+      else if (*p == 'x' || *p == 'X') {
+        while ((tk = *++p) && ((tk >= '0' && tk <= '9') || (tk >= 'a' && tk <= 'f') || (tk >= 'A' && tk <= 'F')))
+          ival = ival * 16 + (tk & 15) + (tk >= 'A' ? 9 : 0);
+      }
+      else { while (*p >= '0' && *p <= '7') ival = ival * 8 + *p++ - '0'; }
+      tk = Num;
+      return;
+    }
+    else if (tk == '/') {
+      if (*p == '/') {
+        ++p;
+        while (*p != 0 && *p != '\n') ++p;
+      }
+      else {
+        tk = Div;
+        return;
+      }
+    }
+    else if (tk == '\'' || tk == '"') {
+      pp = data;
+      while (*p != 0 && *p != tk) {
+        if ((ival = *p++) == '\\') {
+          if ((ival = *p++) == 'n') ival = '\n';
+        }
+        if (tk == '"') *data++ = ival;
+      }
+      ++p;
+      if (tk == '"') ival = (int)pp; else tk = Num;
+      return;
+    }
+    else if (tk == '=') { if (*p == '=') { ++p; tk = Eq; } else tk = Assign; return; }
+    else if (tk == '+') { if (*p == '+') { ++p; tk = Inc; } else tk = Add; return; }
+    else if (tk == '-') { if (*p == '-') { ++p; tk = Dec; } else tk = Sub; return; }
+    else if (tk == '!') { if (*p == '=') { ++p; tk = Ne; } return; }
+    else if (tk == '<') { if (*p == '=') { ++p; tk = Le; } else if (*p == '<') { ++p; tk = Shl; } else tk = Lt; return; }
+    else if (tk == '>') { if (*p == '=') { ++p; tk = Ge; } else if (*p == '>') { ++p; tk = Shr; } else tk = Gt; return; }
+    else if (tk == '|') { if (*p == '|') { ++p; tk = Lor; } else tk = Or; return; }
+    else if (tk == '&') { if (*p == '&') { ++p; tk = Lan; } else tk = And; return; }
+    else if (tk == '^') { tk = Xor; return; }
+    else if (tk == '%') { tk = Mod; return; }
+    else if (tk == '*') { tk = Mul; return; }
+    else if (tk == '[') { tk = Brak; return; }
+    else if (tk == '?') { tk = Cond; return; }
+    else if (tk == '~' || tk == ';' || tk == '{' || tk == '}' || tk == '(' || tk == ')' || tk == ']' || tk == ',' || tk == ':') return;
+  }
+}
